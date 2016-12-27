0ce808b85ffcbb0a98be2cd743418b2d0e56e51e
alan <alwat91@uga.edu>
2016-12-27 11:11:21 -0500

    Created and tested gameEngine.resetGame();

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,38 @@
 var gameEngine = {
-  
+  board:  Array(7).fill(Array(6).fill(null)),
+  player: "b",
+  gameOver: false,
+
+  resetGame: function(){
+    this.board = Array(7).fill(Array(6).fill(null));
+    this.player = "b";
+    this.gameOver = false;
+
+  },
+
+  togglePlayer: function(){
+    // if(player == "r"){
+    //   player
+    // }
+
+  },
+
+  isValidMove: function(){
+
+
+  },
+  checkForVictory: function(position){
+
+
+  },
+  checkLeft: function(position){
+
+  },
+  checkRight: function(position){
+
+  },
+  checkUp: function(position){},
+  checkDown: function(position){},
+  makeMove: function(columnNumber){}
+
 }
